a70a1bd3664465b1dde85c3c288aebeca7551349
aditay <adi.21dec@gmail.com>
2023-02-10 16:37:03 +0530

    Initial commit

---
+++ app/src/main/java/com/aditechnology/tambola/MainActivity.java
@@ -0,0 +1,294 @@
+package com.aditechnology.tambola;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+    MyRecyclerViewAdapter adapter;
+    Button button_restart;
+    TextView clicktext;
+    TextView coinTv;
+    boolean isSpeaking;
+    int last;
+    TextView lastValue;
+    ArrayList<Integer> list;
+    final int max = 89;
+    final int min = 0;
+    /* access modifiers changed from: private */
+    public Switch switch1;
+    private InterstitialAd mInterstitialAd;
+    /* renamed from: t1 */
+    TextToSpeech f47t1;
+    private int mTotalRestartCount;
+
+    public void onItemClick(View view, int i) {
+    }
+
+    private AdView mAdView;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize((Context) this, (OnInitializationCompleteListener) initializationStatus -> {
+
+        });
+        mAdView = findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        loadAds();
+
+
+        this.coinTv = (TextView) findViewById(R.id.coinTv);
+        this.clicktext = (TextView) findViewById(R.id.text);
+
+        TextView textView = (TextView) findViewById(R.id.lastValue);
+        this.lastValue = textView;
+        textView.setVisibility(View.VISIBLE);
+        this.switch1 = (Switch) findViewById(R.id.switch1);
+        this.list = new ArrayList<>();
+        String[] strArr = new String[90];
+        this.button_restart = (Button) findViewById(R.id.button_restart);
+        int i = 0;
+        while (i <= 89) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(RequestConfiguration.MAX_AD_CONTENT_RATING_UNSPECIFIED);
+            int i2 = i + 1;
+            sb.append(i2);
+            strArr[i] = sb.toString();
+            i = i2;
+        }
+        this.button_restart.setOnClickListener(view -> {
+            if (mTotalRestartCount >=1) {
+                //  Toast.makeText(MainActivity.this,"Please watch add to unlock restart",Toast.LENGTH_SHORT);
+                openDialog();
+            }else {
+                mTotalRestartCount = mTotalRestartCount+1;
+                MainActivity.this.list.clear();
+                MainActivity.this.adapter.updateAll();
+                MainActivity.this.isSpeaking = false;
+                MainActivity.this.coinTv.setText("click To Start");
+            }
+            });
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvNumbers);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 10));
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter(this, strArr);
+        this.adapter = myRecyclerViewAdapter;
+        myRecyclerViewAdapter.setClickListener(this);
+        recyclerView.setAdapter(this.adapter);
+        this.f47t1 = new TextToSpeech(getApplicationContext(), i1 -> {
+            if (i1 == 0) {
+                if (MainActivity.this.f47t1.isLanguageAvailable(Locale.US) == 0) {
+                    MainActivity.this.f47t1.setLanguage(Locale.US);
+                }
+            } else if (i1 == -1) {
+                Toast.makeText(MainActivity.this, "Sorry! Text To Speech failed...", Toast.LENGTH_SHORT).show();
+            }
+        });
+        clicktext.setOnClickListener(view -> {
+            if (!MainActivity.this.isSpeaking) {
+                MainActivity.this.isSpeaking = true;
+                MainActivity.this.startGame();
+                MainActivity.this.clicktext.setText("Please wait");
+            }
+        });
+        this.coinTv.setOnClickListener(view -> {
+            if (!MainActivity.this.isSpeaking) {
+                MainActivity.this.isSpeaking = true;
+                MainActivity.this.startGame();
+                MainActivity.this.clicktext.setText("Please wait");
+            }
+        });
+    }
+
+    private void loadAds() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(this,getString(R.string.ads_full_screen_ads), adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("TAG", loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+    }
+
+    public void openDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
+        //Uncomment the below code to Set the message and title from the strings.xml file
+        builder.setMessage("Please watch ads to continue the add").setTitle("Watch Ads");
+
+        //Setting message manually and performing action on button click
+        builder.setMessage("Do you want to watch ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+                    if (mInterstitialAd != null) {
+                        mInterstitialAd.show(MainActivity.this);
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when fullscreen content is dismissed.
+                                Log.d("TAG", "The ad was dismissed.");
+                                mTotalRestartCount =0;
+                            }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when fullscreen content failed to show.
+                                Log.d("TAG", "The ad failed to show.");
+                                mTotalRestartCount =0;
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when fullscreen content is shown.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                mInterstitialAd = null;
+                                mTotalRestartCount =0;
+                                loadAds();
+                                Log.d("TAG", "The ad was shown.");
+                            }
+                        });
+                    } else {
+                        mTotalRestartCount =0;
+                        Toast.makeText(MainActivity.this,"Ads not prepared yet!!Enjoy our free service",Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "The interstitial ad wasn't ready yet.");
+                    }
+                    dialog.cancel();
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        dialog.cancel();
+
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("Watch Ads to restart the game");
+        alert.show();
+    }
+
+    /* access modifiers changed from: private */
+    public void startGame() {
+        char c;
+        char c2;
+        if (this.list.size() < 90) {
+            int random = getRandom();
+            if (this.last != 0) {
+                this.lastValue.setVisibility(View.VISIBLE);
+                TextView textView = this.lastValue;
+                textView.setText("" + (this.last + 1));
+            }
+            this.last = random;
+            TextView textView2 = this.coinTv;
+            StringBuilder sb = new StringBuilder();
+            sb.append(RequestConfiguration.MAX_AD_CONTENT_RATING_UNSPECIFIED);
+            int i = random + 1;
+            sb.append(i);
+            textView2.setText(sb.toString());
+            String num = Integer.toString(i);
+            if (i > 9) {
+                c = num.charAt(0);
+                c2 = num.charAt(1);
+            } else {
+                c = num.charAt(0);
+                c2 = 0;
+            }
+            int numericValue = Character.getNumericValue(c);
+            int numericValue2 = Character.getNumericValue(c2);
+            if (!this.switch1.isChecked()) {
+                TextToSpeech textToSpeech = this.f47t1;
+                textToSpeech.speak(RequestConfiguration.MAX_AD_CONTENT_RATING_UNSPECIFIED + numericValue, 0, (HashMap) null);
+                if (i > 9) {
+                    speakSecond(numericValue2, random);
+                } else {
+                    this.clicktext.setText("Click Here");
+                    this.isSpeaking = false;
+                }
+            } else {
+                this.isSpeaking = false;
+                this.clicktext.setText("Click Here");
+            }
+            this.adapter.updateView(random);
+            return;
+        }
+        this.coinTv.setText("Game Over");
+    }
+
+    private void speakSecond(final int i, final int i2) {
+        new Handler().postDelayed(() -> {
+            if (!MainActivity.this.switch1.isChecked()) {
+                TextToSpeech textToSpeech = MainActivity.this.f47t1;
+                textToSpeech.speak(RequestConfiguration.MAX_AD_CONTENT_RATING_UNSPECIFIED + i, 0, (HashMap) null);
+            }
+            MainActivity.this.speakFinal(i2);
+        }, 1000);
+    }
+
+    /* access modifiers changed from: private */
+    public void speakFinal(final int i) {
+        new Handler().postDelayed(() -> {
+            if (!MainActivity.this.switch1.isChecked()) {
+                TextToSpeech textToSpeech = MainActivity.this.f47t1;
+                textToSpeech.speak(RequestConfiguration.MAX_AD_CONTENT_RATING_UNSPECIFIED + (i + 1), 0, (HashMap) null);
+            }
+            MainActivity.this.isSpeaking = false;
+            MainActivity.this.clicktext.setText("Click Here");
+        }, 1000);
+    }
+
+    private int getRandom() {
+        int nextInt = new Random().nextInt(90) + 0;
+        if (this.list.contains(Integer.valueOf(nextInt))) {
+            return getRandom();
+        }
+        this.list.add(Integer.valueOf(nextInt));
+        return nextInt;
+    }
+}
---
+++ app/src/main/java/com/aditechnology/tambola/MyRecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package com.aditechnology.tambola;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+    private SparseBooleanArray isSelected = new SparseBooleanArray();
+    /* access modifiers changed from: private */
+    public ItemClickListener mClickListener;
+    private String[] mData;
+    private LayoutInflater mInflater;
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int i);
+    }
+
+    MyRecyclerViewAdapter(Context context, String[] strArr) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = strArr;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return new ViewHolder(this.mInflater.inflate(R.layout.recyclerview_item, viewGroup, false));
+    }
+
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        if (this.isSelected.get(i)) {
+            viewHolder.layout.setBackgroundColor(ContextCompat.getColor(viewHolder.layout.getContext(), R.color.colorAccent));
+        } else if (Build.VERSION.SDK_INT >= 16) {
+            viewHolder.layout.setBackground(ContextCompat.getDrawable(viewHolder.layout.getContext(), R.drawable.border));
+        } else {
+            viewHolder.layout.setBackgroundResource(R.drawable.border);
+        }
+        viewHolder.myTextView.setText(this.mData[i]);
+    }
+
+    public int getItemCount() {
+        return this.mData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        LinearLayout layout;
+        TextView myTextView;
+
+        ViewHolder(View view) {
+            super(view);
+            this.myTextView = (TextView) view.findViewById(R.id.info_text);
+            this.layout = (LinearLayout) view.findViewById(R.id.layout);
+            view.setOnClickListener(this);
+        }
+
+        public void onClick(View view) {
+            if (MyRecyclerViewAdapter.this.mClickListener != null) {
+                MyRecyclerViewAdapter.this.mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+    }
+
+    /* access modifiers changed from: package-private */
+    public String getItem(int i) {
+        return this.mData[i];
+    }
+
+    /* access modifiers changed from: package-private */
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    /* access modifiers changed from: protected */
+    public void updateView(int i) {
+        this.isSelected.put(i, true);
+        notifyDataSetChanged();
+    }
+
+    /* access modifiers changed from: protected */
+    public void updateAll() {
+        this.isSelected.clear();
+        notifyDataSetChanged();
+    }
+}
